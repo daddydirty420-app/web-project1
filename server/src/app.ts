b72ad3620300201930c583262ad5d31a2d4d98ec
daddydirty420-app <daddydirty420@gmail.com>
2025-11-07 20:44:08 +0900

    fix: disable unused vars for vercel build

--- server/src/app.ts
+++ server/src/app.ts
@@ -97,7 +97,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: "https://exclude-consultancy-boxed-posts.trycloudflare.com",
   credentials: true
 }));
 
