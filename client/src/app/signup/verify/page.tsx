974ddcf32facd348a53180f56a60923a04ae99cb
daddydirty420-app <daddydirty420@gmail.com>
2025-11-07 18:25:40 +0900

    fix: disable unused vars for vercel build

--- client/src/app/signup/verify/page.tsx
+++ client/src/app/signup/verify/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { Container } from "@/components";
 import VerifyForm from './verify-form';
 import Resend from "./resend";
 import { Metadata } from "next";
-import styles from '@/styles/login.module.css'
+import styles from '@/styles/login.module.css';
 
 export const metadata: Metadata = {
     title: "メール認証 | FLEX OUTDOOR",
