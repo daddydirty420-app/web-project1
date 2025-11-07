669a60849db36284dc657830a3c61d9380bb2207
daddydirty420-app <daddydirty420@gmail.com>
2025-11-07 19:14:31 +0900

    fix: disable unused vars for vercel build

--- client/src/app/signup/verify/page.tsx
+++ client/src/app/signup/verify/page.tsx
@@ -5,7 +5,7 @@ import VerifyForm from './verify-form';
 import Resend from "./resend";
 import styles from '@/styles/login.module.css';
 
-export default function Verify() {
+export default function Page() {
     return (
         <Container>
             <h1 className={styles.title}>メール認証</h1>
