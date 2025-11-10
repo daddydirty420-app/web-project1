e3e38be9e22aabbbcc8bbf5702971181c7358d22
daddydirty420-app <daddydirty420@gmail.com>
2025-11-10 14:10:25 +0900

    update

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
 }
 
 export default function Login() {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
     return (
         <Container>
             <h1 className={styles.title}>ログイン</h1>
