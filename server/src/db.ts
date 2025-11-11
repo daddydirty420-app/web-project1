320760e6c252f7f258ff79fc633d3f51298956a4
daddydirty420-app <daddydirty420@gmail.com>
2025-11-11 18:01:21 +0900

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,17 +2,23 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProd = process.env.NODE_ENV === "production";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set!");
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production'
-    ? {
+  dialectOptions: isProd 
+  ? {
+    ssl: {
       require: true,
       rejectUnauthorized: false
-    }
-    : false
+    },
   }
+  : {},
 });
 
 export default sequelize;
