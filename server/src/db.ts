eed3b6a094449428acad685460a763f023c0efc7
daddydirty420-app <daddydirty420@gmail.com>
2025-11-11 18:51:00 +0900

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -13,8 +13,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   logging: false,
   dialectOptions: {
     ssl: {
-      require: true,
-      rejectUnauthorized: false
+      require: true
     },
   },
 });
