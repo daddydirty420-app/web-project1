cd18c6ef83a9c0750d87c079721ec3cce5f232c4
daddydirty420-app <daddydirty420@gmail.com>
2025-11-11 18:32:05 +0900

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -11,14 +11,12 @@ if (!process.env.DATABASE_URL) {
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
   logging: false,
-  dialectOptions: isProd 
-  ? {
+  dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     },
-  }
-  : {},
+  },
 });
 
 export default sequelize;
