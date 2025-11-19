db32fd97c1dc11fb379d5c94481e588be2a571ec
daddydirty420-app <daddydirty420@gmail.com>
2025-11-19 19:52:24 +0900

    update

--- client/src/app/shop-signup/1/page.tsx
+++ client/src/app/shop-signup/1/page.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { notFound, redirect } from "next/navigation";
 import Form from "./form";
+import { cookies } from "next/headers";
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
@@ -16,9 +17,10 @@ export async function generateMetadata(): Promise<Metadata> {
 };
 
 export default async function Page() {
-    const session = await getServerSession(authOptions);
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("next-auth.session-token")?.value;
 
-    if (!session?.accessToken) {
+    if (!accessToken) {
         redirect("/login");
     }
 
@@ -26,7 +28,7 @@ export default async function Page() {
         method: "GET",
         cache: "no-store",
         headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
+            Authorization: `Bearer ${accessToken}`,
         },
     });
 
@@ -42,7 +44,7 @@ export default async function Page() {
 
     return (
         <Form
-        session={session}
+        session={null}
         user={data.data}
         shopInfo={data.shopData}
         ComOrFreeOption={data.comOrFree}
