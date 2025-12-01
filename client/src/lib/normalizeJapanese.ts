95f372cc360ee1c805ee98b3a7229bcf49140ce8
daddydirty420-app <daddydirty420@gmail.com>
2025-12-01 12:36:10 +0900

    update

--- client/src/lib/normalizeJapanese.ts
+++ client/src/lib/normalizeJapanese.ts
@@ -1,30 +1,21 @@
-const toHiragana = (str: string) =>
-    str.replace(/[\u30A0-\u30FF]/g, (ch) =>
+export const normalizeJapanese = (str: string) => {
+    let s = str.toLowerCase();
+    
+    s = s.replace(/[\u30A0-\u30FF]/g, (ch) =>
         String.fromCharCode(ch.charCodeAt(0) - 0x60)
     )
     .replace(/ヵ/g, "か")
     .replace(/ヶ/g, "け");
-
-const removeDakuten = (str: string) =>
-    str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
-const normalizeSmallKana = (str: string) =>
-    str
+    
+    s = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    
+    s = s
     .replace(/ゃ/g, "や")
     .replace(/ゅ/g, "ゆ")
     .replace(/ょ/g, "よ")
     .replace(/っ/g, "つ");
-
-const absorbChoon = (str: string) => str.replace(/ー/g, "");
-
-export const normalizeJapanese = (str: string) => {
-    let s = str;
-
-    s = s.toLowerCase();
-    s = toHiragana(s);
-    s = removeDakuten(s);
-    s = normalizeSmallKana(s);
-    s = absorbChoon(s);
+    
+    s = s.replace(/ー/g, "");
 
     return s;
 };
