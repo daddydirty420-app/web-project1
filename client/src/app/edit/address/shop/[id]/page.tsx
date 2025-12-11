02d3b49cc432721c96a1f8926dd94137e2b5587a
daddydirty420-app <daddydirty420@gmail.com>
2025-12-11 18:53:45 +0900

    update

--- client/src/app/edit/address/shop/[id]/page.tsx
+++ client/src/app/edit/address/shop/[id]/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata(): Promise<Metadata> {
 };
 
 export default async function Page({ params }: Props) {
-    const { id } = params;
+    const { id } = await params;
 
     const cookieStore = await cookies();
     const accessToken = cookieStore.get("access-token")?.value;
