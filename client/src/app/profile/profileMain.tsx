250a8321af2a8bc014394fbf00b08de6dcae9814
daddydirty420-app <daddydirty420@gmail.com>
2025-12-26 11:09:19 +0900

    update

--- client/src/app/profile/profileMain.tsx
+++ client/src/app/profile/profileMain.tsx
@@ -24,7 +24,10 @@ export default function ProfileMain({ data, userId, currentUserId, adminPage, lo
         <section className={styles.profileBlock}>
             <div className={styles.userDiv}>
                 <Image
-                src={data.user.profile_image || '/default-profile.png'}
+                src={data.user.profile_image
+                    ? encodeURI(data.user.profile_image)
+                    : '/default-profile.png'
+                }
                 alt='プロフィール画像'
                 width={60}
                 height={60}
@@ -32,22 +35,29 @@ export default function ProfileMain({ data, userId, currentUserId, adminPage, lo
                 quality={75}
                 className={styles.profileImage}
                 />
-                <h1 className={styles.userName}>{data.user.user_name}</h1>
-                {data.user.honnin_verified && (
-                    <FontAwesomeIcon icon={faCircleCheck} className={styles.honninIcon} />
-                )}
-                {data.user.early_seller && (
-                    <FontAwesomeIcon icon={faTag} className={styles.earlyIcon} />
-                )}
-                {data.hasShop && (
-                    <FontAwesomeIcon icon={faStore} className={styles.shopIcon} />
-                )}
+
+                <div className={styles.nameBlock}>
+                    <h1 className={styles.userName}>{data.user.user_name}</h1>
+
+                    <div className={styles.iconRow}>
+                        {data.user.honnin_verified && (
+                            <FontAwesomeIcon icon={faCircleCheck} className={styles.honninIcon} />
+                        )}
+                        {data.user.early_seller && (
+                            <FontAwesomeIcon icon={faTag} className={styles.earlyIcon} />
+                        )}
+                        {data.hasShop && (
+                            <FontAwesomeIcon icon={faStore} className={styles.shopIcon} />
+                        )}
+                    </div>
+                </div>
             </div>
+
             {loggedIn && !sameId && !adminPage && <FollowButton targetUserId={userId} currentUserId={currentUserId} />}
             {loggedIn && sameId && !adminPage && <EditButton />}
         </section>
 
         <Introduction data={data} />
         </>
-    )
+    );
 }
