d839cd2430ec1148ece87ac7c25a2f3a7dedc394
daddydirty420-app <daddydirty420@gmail.com>
2026-01-05 19:17:56 +0900

    update

--- server/src/types/custom.d.ts
+++ server/src/types/custom.d.ts
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 declare module 'express';
 declare module 'jsonwebtoken';
 declare module 'bcrypt';
@@ -8,10 +6,6 @@ declare module 'uuid';
 declare module 'bcrypt';
 declare module 'multer';
 declare module 'http-errors';
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 declare module 'bcrypt';
 declare module 'multer';
 declare module 'http-errors';
