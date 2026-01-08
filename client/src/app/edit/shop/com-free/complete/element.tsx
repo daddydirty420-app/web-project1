f0ceae843182ad1c16d945c7d9b6c6ec9a4c5868
daddydirty420-app <daddydirty420@gmail.com>
2026-01-08 15:06:01 +0900

    update

--- client/src/app/edit/shop/com-free/complete/element.tsx
+++ client/src/app/edit/shop/com-free/complete/element.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Element() {
     return (
         <Container>
-            <h1 className={styles.title}>事業形態変更リクエスト完了</h1>
+            <h1 className={styles.title}>事業形態変更の申請が完了しました</h1>
 
             <p className={styles.content}>事業形態変更のお申込みが完了しました。
                 <br />審査完了後にメールにて改めて事業形態の変更をお知らせいたします。なお、審査には1～2週間ほどお時間を頂戴しております。ご迷惑をお掛けしますが、ご協力お願いいたします。
