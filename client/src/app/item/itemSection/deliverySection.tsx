565eca461f40d2f12faaae854fd428f1b3bd046f
daddydirty420-app <daddydirty420@gmail.com>
2026-01-12 20:13:08 +0900

    update

--- client/src/app/item/itemSection/deliverySection.tsx
+++ client/src/app/item/itemSection/deliverySection.tsx
@@ -1,6 +1,5 @@
 import styles from "./item.module.css";
 import { Item } from "../itemPageTypes";
-import clsx from "clsx";
 
 type Props = {
     item: Item;
@@ -11,36 +10,36 @@ export default function DeliverySection({ item }: Props) {
     return (
         <section className={styles.deliverySection}>
             <p className={styles.syohinText}>商品の情報</p>
-            <div className={styles.infoFlex}>
-                <p className={styles.infoText}>商品の状態：</p>
-                <p className={styles.infoData}>{item.ItemConditionOption?.name}</p>
+            <div className={styles.infoRow}>
+                <span className={styles.label}>商品の状態</span>
+                <span className={styles.value}>{item.ItemConditionOption?.name}</span>
             </div>
-            <div className={styles.infoFlex}>
-                <p className={styles.infoText}>配送の方法：</p>
-                <p className={styles.infoData}>{item.ItemShippingProfile?.ShippingServiceOption?.name}</p>
+            <div className={styles.infoRow}>
+                <span className={styles.label}>配送の方法</span>
+                <span className={styles.value}>{item.ItemShippingProfile?.ShippingServiceOption?.name}</span>
             </div>
-            <div className={styles.infoFlex}>
-                <p className={styles.infoText}>発送元地域：</p>
-                <p className={styles.infoData}>{item.ItemShippingProfile?.TodouhukenOption?.name}</p>
+            <div className={styles.infoRow}>
+                <span className={styles.label}>発送元地域</span>
+                <span className={styles.value}>{item.ItemShippingProfile?.TodouhukenOption?.name}</span>
             </div>
-            <div className={styles.infoFlex}>
-                <p className={clsx(styles.infoText, styles.font13)}>発送までの日数：</p>
-                <p className={styles.infoData}>{item.ItemShippingProfile?.ShippingDayOption?.name}</p>
+            <div className={styles.infoRow}>
+                <span className={styles.label}>発送までの日数</span>
+                <span className={styles.value}>{item.ItemShippingProfile?.ShippingDayOption?.name}</span>
             </div>
             {inventory && inventory?.initial >= 2 && (
-                <div className={styles.infoFlex}>
-                    <p className={styles.infoText}>在庫：</p>
+                <div className={styles.infoRow}>
+                    <span className={styles.label}>在庫</span>
                     {inventory.initial <= 4
                     ? inventory.current >= 2
                     : inventory.initial / inventory.current > inventory.low_stock_ratio
-                    && <p className={clsx(styles.infoData, "text-[var(--theme)]")}>在庫あり</p>}
+                    && <span className={`${styles.value} ${styles.full}`}>在庫あり</span>}
 
                     {inventory.initial <= 4
                     ? inventory.current === 1
                     : (inventory.current / inventory.initial <= inventory.low_stock_ratio && inventory.current >= 1)
-                    && <p className={clsx(styles.infoData, "text-[var(--alert)]")}>残りわずか</p>}
+                    && <span className={`${styles.value} ${styles.low}`}>残りわずか</span>}
 
-                    {inventory.current === 0 && <p className={clsx(styles.infoData, "text-[var(--alert)]")}>SOLD OUT</p>}
+                    {inventory.current === 0 && <span className={`${styles.value} ${styles.sold}`}>SOLD OUT</span>}
                 </div>
             )}
         </section>
