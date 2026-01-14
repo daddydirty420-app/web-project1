af6f31ad6dba40c47b7b147b6c82b27cafcde46b
daddydirty420-app <daddydirty420@gmail.com>
2026-01-14 17:16:15 +0900

    update

--- client/src/providers/toastBoundary.tsx
+++ client/src/providers/toastBoundary.tsx
@@ -5,7 +5,18 @@ import { Toaster } from "react-hot-toast";
 export const ToastBoundary = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-        <Toaster position="top-center" />
+        <Toaster
+        position="top-center"
+        toastOptions={{
+            duration: 2500,
+            style: {
+                background: "#111",
+                color: "#fff",
+                fontSize: "13px",
+                borderRadius: "8px",
+            },
+        }}
+        />
         {children}
         </>
     );
