2fa317f06a6a6865dd2df4ccbe9158ad026950e1
daddydirty420-app <daddydirty420@gmail.com>
2026-02-16 19:49:50 +0900

    update

---
+++ client/src/app/upload/draft/[id]/page.tsx
@@ -0,0 +1,76 @@
+import { Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { cookies } from "next/headers";
+import { Form } from "../../form";
+
+type Props = {
+    params: { id: string };
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "下書きを編集する",
+        description: "下書き保存した商品情報の変更はこちら！",
+        robots: {
+            index: false,
+            follow: false
+        }
+    };
+};
+
+export default async function Page({ params }: Props) {
+    const { id } = await params;
+
+    const session = await getServerSession(authOptions);
+
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("access-token")?.value;
+    
+    if (!accessToken) redirect("/login");
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/item-upload/upload-edit-draft/${id}?page=draft`, {
+        method: "GET",
+        cache: "no-store",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    if (!res.ok) {
+        notFound();
+    }
+
+    const data = await res.json();
+    const item = data.item;
+    const category = data.category;
+    const allCondition = data.allCondition;
+    const allDay = data.allDay;
+    const allService = data.allService;
+    const allPlace = data.allPlace;
+    const hasShop = data.hasShop;
+
+    const userId = String(session?.user.id).trim();
+    const sellerId = String(item.seller_id).trim();
+
+    if (userId !== sellerId) {
+        redirect("/item-list/draft");
+    }
+
+    if (item.status !== "draft") {
+        redirect(`/item/${id}`);
+    }
+
+    return <Form
+    itemId={id}
+    item={item}
+    category={category}
+    allCondition={allCondition}
+    allDay={allDay}
+    allService={allService}
+    allPlace={allPlace}
+    hasShop={hasShop}
+    page="draft"
+    />;
+}
