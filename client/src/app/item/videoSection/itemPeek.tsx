7e528caaf0cf9703cd0b70131ed7741b930ebdff
daddydirty420-app <daddydirty420@gmail.com>
2026-02-17 16:45:56 +0900

    update

--- client/src/app/item/videoSection/itemPeek.tsx
+++ client/src/app/item/videoSection/itemPeek.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { Item } from "../itemPageTypes";
-import styles from "./video.module.css";
+import styles from "./peek.module.css";
 
 type Props = {
     item: Item;
@@ -24,19 +24,45 @@ export const ItemPeek = ({ item }: Props) => {
         role="button"
         aria-label="商品説明へスクロール"
         >
-            <div className={styles.peekImageName}>
+            <div className={styles.imageName}>
                 <Image
                 src={item.first_image_url}
                 alt="商品画像1枚目"
                 width={45}
                 height={45}
-                className={styles.peekImage}
+                className={styles.image}
                 />
 
-                <p className={styles.peekName}>{item.name}</p>
+                <div className={styles.nameType}>
+                    <p className={styles.name}>{item.name}</p>
+
+                    {(
+                        ["men", "women", "unisex"].includes(item.gender_type) ||
+                        item.age_type === "kids"
+                    ) && (
+                        <div className={styles.typeRow}>
+                            {item.gender_type === "men" && (
+                                <span className={styles.typeText}>メンズ</span>
+                            )}
+                            {item.gender_type === "women" && (
+                                <span className={styles.typeText}>レディース</span>
+                            )}
+                            {item.gender_type === "unisex" && (
+                                <span className={styles.typeText}>ユニセックス</span>
+                            )}
+
+                            {item.age_type === "kids" && (
+                                <>
+                                <span className={styles.typeText}>/</span>
+                                <span className={styles.typeText}>キッズ</span>
+                                </>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
 
-            <p className={`${styles.peekPrice}
+            <p className={`${styles.price}
             ${item.status === "soldout" ? `${styles.sold}` : ""}`}>
                 {item.status === "soldout" ? "SOLD OUT" : `￥${item.price.toLocaleString()}`}
             </p>
