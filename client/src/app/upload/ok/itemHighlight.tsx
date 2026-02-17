735424995d59b54bbcde4fadec0840913ac0bdf2
daddydirty420-app <daddydirty420@gmail.com>
2026-02-17 20:14:55 +0900

    update

---
+++ client/src/app/upload/ok/itemHighlight.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Item } from "../types/type";
+import styles from "./ok.module.css";
+
+type Props = {
+    itemId: string;
+    item: Item;
+    hasReccomend: boolean;
+};
+
+export const ItemHighlight = ({ itemId, item, hasReccomend }: Props) => {
+    return (
+        <></>
+    );
+};
