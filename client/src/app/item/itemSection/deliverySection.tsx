21c4e618d520c3ea5591228d240b2c453f8783a7
daddydirty420-app <daddydirty420@gmail.com>
2026-02-17 14:15:25 +0900

    update

--- client/src/app/item/itemSection/deliverySection.tsx
+++ client/src/app/item/itemSection/deliverySection.tsx
@@ -7,25 +7,32 @@ type Props = {
 
 export const DeliverySection = ({ item }: Props) => {
     const inventory = item.attributes.inventory;
+    const materials = item.attributes.materials;
+
     return (
         <section className={styles.deliverySection}>
             <p className={styles.syohinText}>商品の情報</p>
+
             <div className={styles.infoRow}>
                 <span className={styles.label}>商品の状態</span>
                 <span className={styles.value}>{item.ItemConditionOption?.name}</span>
             </div>
+
             <div className={styles.infoRow}>
                 <span className={styles.label}>配送の方法</span>
                 <span className={styles.value}>{item.ItemShippingProfile?.ShippingServiceOption?.name}</span>
             </div>
+
             <div className={styles.infoRow}>
                 <span className={styles.label}>発送元地域</span>
                 <span className={styles.value}>{item.ItemShippingProfile?.TodouhukenOption?.name}</span>
             </div>
+
             <div className={styles.infoRow}>
                 <span className={styles.label}>発送までの日数</span>
                 <span className={styles.value}>{item.ItemShippingProfile?.ShippingDayOption?.name}</span>
             </div>
+
             {inventory && inventory.initial >= 2 && (
                 <div className={styles.infoRow}>
                     <span className={styles.label}>在庫</span>
@@ -40,6 +47,23 @@ export const DeliverySection = ({ item }: Props) => {
                     && <span className={`${styles.value} ${styles.low}`}>残りわずか</span>}
 
                     {inventory.current === 0 && <span className={`${styles.value} ${styles.sold}`}>SOLD OUT</span>}
+                </div>
+            )}
+
+            {materials && materials.length > 0 && (
+                <div className={styles.infoRow}>
+                    <span className={styles.label}>素材</span>
+
+                    <div className={styles.materialColumn}>
+                        {materials.map((m, i) => (
+                                <div key={i} className={styles.materialRow}>
+                                    <span className={styles.value}>{m.name}</span>
+                                    <span className={styles.materialBar}>-</span>
+                                    <span className={styles.value}>{m.ratio}%</span>
+                                </div>
+                            )
+                        )}
+                    </div>
                 </div>
             )}
         </section>
