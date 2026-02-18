23a08868c1c59f70f6a9c6da5fd6903fb108e2fa
daddydirty420-app <daddydirty420@gmail.com>
2026-02-18 19:51:10 +0900

    update

--- client/src/app/upload/ok/okText.tsx
+++ client/src/app/upload/ok/okText.tsx
@@ -7,11 +7,12 @@ type Props = {
 export const OkText = ({ name }: Props) => {
     return (
         <div className={styles.okTextDiv}>
+            <div className={styles.icon}>✓</div>
             <p className={styles.okText}>
                 <span className={styles.bold}>{name}</span>
                 の出品が完了しました。
-                ただいまより商品の販売が開始されます。
             </p>
+            <p className={styles.subText}>ただいまより商品の販売が開始されます。</p>
         </div>
     );
 };
