a085572d0f23766db889b66f5061ea5bed3ed3e6
daddydirty420-app <daddydirty420@gmail.com>
2026-02-20 16:08:00 +0900

    update

--- client/src/app/upload/ok/okUI.tsx
+++ client/src/app/upload/ok/okUI.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function OkUI({ title, children }: Props) {
     return (
-        <Container header>
+        <Container>
             <Title title={title} />
             {children}
         </Container>
