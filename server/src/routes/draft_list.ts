63bb4a1bb912e740b90f1e92e4d8ce76f9961f77
daddydirty420-app <daddydirty420@gmail.com>
2026-03-04 20:40:37 +0900

    update

--- server/src/routes/draft_list.ts
+++ server/src/routes/draft_list.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import type { Request, Response } from "express-serve-static-core";
 import { authenticateToken, authenticateOptional } from "../middleware/index.js";
 import { Op, literal, WhereOptions } from "sequelize";
-import { Item, User, Video, Sale, Search } from "../models/index.js";
+import { Item, User, Video, Sale, Search, Categories } from "../models/index.js";
 import { normalizeJapanese } from "../utils/normalizeJapanese.js";
 import sequelize from "../db.js";
 
@@ -74,6 +74,59 @@ router.get('/search', authenticateToken, async (req: Request, res: Response): Pr
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'サーバーエラーが発生しました。' });
+    }
+});
+
+router.patch("/search/:id", async (req: Request, res: Response): Promise<void> => {
+    const itemId = req.params.id;
+
+    const keyword = req.query?.keyword ?? "";
+    if (!String(keyword).trim()) {
+        res.status(400).json({ message: "検索キーワードがありません" });
+        return;
+    }
+
+    try {
+            const item = await Item.findByPk(itemId, {
+                include: [
+                    { model: Video },
+                    {
+                        model: Categories,
+                        as: "Category",
+                        include: [
+                            {
+                                model: Categories,
+                                as: "parent",
+                                required: false,
+                            },
+                        ],
+                    },
+                    { model: User },
+                ],
+            });
+            if (!item) {
+                res.status(404).json({ message: "itemが見つかりません。" });
+                return;
+            }
+
+        const searchText = `
+        ${item.name}
+        ${item.Video?.title ?? ""}
+        ${item.Category?.name ?? ""}
+        ${item.Category?.parent?.name ?? ""}
+        ${item.User?.user_name ?? ""}
+        `;
+
+        const normalizeSearchText = normalizeJapanese(searchText ?? "");
+
+        await item.update({
+            search_text: normalizeSearchText,
+        });
+
+        res.status(200).json({ message: "成功" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "サーバーエラーが発生しました。" });
     }
 });
 
