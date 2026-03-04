8882da947c30f693bd7054c1d89e1fa8f33e35e1
daddydirty420-app <daddydirty420@gmail.com>
2026-03-04 16:30:14 +0900

    update

--- server/src/routes/draft_list.ts
+++ server/src/routes/draft_list.ts
@@ -3,6 +3,8 @@ import type { Request, Response } from "express-serve-static-core";
 import { authenticateToken, authenticateOptional } from "../middleware/index.js";
 import { Op, literal, WhereOptions } from "sequelize";
 import { Item, User, Video, Sale, Search } from "../models/index.js";
+import { normalizeJapanese } from "utils/normalizeJapanese.js";
+import sequelize from "../db.js";
 
 const router = Router();
 
@@ -34,6 +36,70 @@ router.get('/', authenticateToken, async (req: Request, res: Response): Promise<
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'サーバーエラーが発生しました。' });
+    }
+});
+
+router.get('/search', authenticateToken, async (req: Request, res: Response): Promise<void> => {
+    const currentUserId = req.user!.id;
+
+    const keyword = normalizeJapanese((req.query.keyword ?? "") as string);
+    if (!keyword) {
+        res.status(400).json({ message: "検索キーワードがありません" });
+        return;
+    }
+
+    try {
+        const itemList = await Item.findAll({
+            attributes: ['id', 'name', 'price', "status", 'seller_id', 'save_at', 'first_image_url', "gender_type", "age_type"],
+            where: {
+                seller_id: currentUserId,
+                status: "draft",
+                search_text: { [Op.iLike]: `%${keyword}%` },
+            },
+            order: [['save_at', 'DESC']],
+            include: [
+                {
+                    model: Video,
+                    attributes: ['title'],
+                },
+            ],
+        });
+
+        if (!itemList) {
+            res.status(404).json({ message: 'アイテムが見つかりません。' });
+            return;
+        }
+
+        res.status(200).json({ itemList });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'サーバーエラーが発生しました。' });
+    }
+});
+
+router.patch("/search-text", async (req: Request, res: Response): Promise<void> => {
+    const t = await sequelize.transaction();
+    try {
+        const items = await Item.findAll();
+
+        if (!items) {
+            throw new Error("Item見つからない");
+        }
+
+        await Promise.all(items.map(async (item: typeof Item) => {
+            const searchText = normalizeJapanese(item.search_text);
+
+            await item.update({
+                search_text: searchText,
+            }, { transaction: t });
+        }));
+
+        await t.commit();
+
+        console.log("search_text更新");
+    } catch (err) {
+        await t.rollback();
+        console.log("search_text更新失敗：", err);
     }
 });
 
--- server/src/utils/normalizeJapanese.ts
+++ server/src/utils/normalizeJapanese.ts
@@ -1,27 +1,13 @@
 export const normalizeJapanese = (str: string) => {
-    let s = str.toLowerCase();
-    
+    let s = str
+    .normalize("NFKC")
+    .toLowerCase();
+
     s = s.replace(/[\u30A0-\u30FF]/g, (ch) =>
         String.fromCharCode(ch.charCodeAt(0) - 0x60)
-    )
-    .replace(/ヵ/g, "か")
-    .replace(/ヶ/g, "け");
-    
-    s = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    
-    s = s
-    .replace(/ゃ/g, "や")
-    .replace(/ゅ/g, "ゆ")
-    .replace(/ょ/g, "よ")
-    .replace(/っ/g, "つ");
-    
-    s = s.replace(/ー/g, "");
-
-    s = s
-    .replace(/\s+/g, "") // スペース削除
-    .replace(/[Ａ-Ｚａ-ｚ０-９]/g, ch =>
-        String.fromCharCode(ch.charCodeAt(0) - 0xFEE0) // 全角→半角
     );
+
+    s = s.replace(/\s+/g, "");
 
     return s;
 };
