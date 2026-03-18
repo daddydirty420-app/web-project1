3920420f02aa8c2b7d9445e3723ae68acd1fb3e9
daddydirty420-app <daddydirty420@gmail.com>
2026-03-18 19:16:37 +0900

    fix: apiUrl変更

--- client/src/app/lp/page.tsx
+++ client/src/app/lp/page.tsx
@@ -28,7 +28,7 @@ export default async function Page() {
 
     const defaultLimit = 15;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/item-list/index-item-list/video-list?page=1&limit=${defaultLimit}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/items?type=video&page=1&view=index&limit=${defaultLimit}`, {
         method: 'GET',
         headers: {
             Authorization: `Bearer ${accessToken ?? ""}`,
