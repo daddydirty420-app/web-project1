8fa7d3381f08d3c96188747ec1d78771774ac97e
daddydirty420-app <newproject893420@gmail.com>
2026-05-14 13:54:06 +0900

    add: searchRateLimit

--- server/src/routes/search.ts
+++ server/src/routes/search.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import type { NextFunction, Request, Response } from "express-serve-static-core";
 import { authenticateToken } from "../middleware/authMiddleware.js";
+import { getSearchHistoryRateLimit } from "../middleware/rateLimit/searchRateLimit.js";
 import { getSearchHistoryUseCase } from "../usecases/search/getSearchHistory.js";
 
 const router = Router();
@@ -8,16 +9,21 @@ const router = Router();
 // GET /search/history
 // summary: 検索履歴取得
 // page: header
-router.get("/history", authenticateToken, async (req: Request, res: Response, next: NextFunction) => {
-    const userId = req.user!.id;
+router.get(
+    "/history",
+    getSearchHistoryRateLimit,
+    authenticateToken,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.user!.id;
 
-    try {
-        const sortedData = await getSearchHistoryUseCase({ userId });
+        try {
+            const sortedData = await getSearchHistoryUseCase({ userId });
 
-        res.status(200).json({ sortedData });
-    } catch (err) {
-        next(err);
-    }
-});
+            res.status(200).json({ sortedData });
+        } catch (err) {
+            next(err);
+        }
+    },
+);
 
 export default router;
