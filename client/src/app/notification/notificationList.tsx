4c085c5660afb5c8487fdf765d8b07e683f8f898
daddydirty420-app <newproject893420@gmail.com>
2026-05-17 19:38:55 +0900

    add: NotificationList state router

--- client/src/app/notification/notificationList.tsx
+++ client/src/app/notification/notificationList.tsx
@@ -1,1 +1,22 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Notification } from "./type";
+
+type Response = {
+    notificationList: Notification[];
+    unreadCount: number;
+};
+
+export const NotificationList = () => {
+    const [popup, setPopup] = useState(false);
+
+    const router = useRouter();
+
+    // APIフェッチ
+
+    return (
+        <></>
+    );
+};
