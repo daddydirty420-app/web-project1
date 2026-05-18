66e119059b48c31021932d62ec5ce6536370f32e
daddydirty420-app <newproject893420@gmail.com>
2026-05-18 13:08:26 +0900

    add: /client api/client.ts クライアント共通API fetch

---
+++ client/src/lib/api/client.ts
@@ -0,0 +1,30 @@
+import { getAccessToken } from "../getAccessToken";
+
+type FetchOptions = RequestInit & {
+    token?: string;
+};
+
+export const apiFetch = async <T>(path: string, options: FetchOptions = {}): Promise<T> => {
+    const accessToken = await getAccessToken();
+
+    if (!accessToken) {
+        throw new Error("UNAUTHORIZED");
+    }
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${path}`, {
+        ...options,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+            ...options.headers,
+        },
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.code ?? "API Error");
+    }
+
+    return data;
+};
