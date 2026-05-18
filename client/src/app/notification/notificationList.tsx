64ff54d1937c9cf25ce86b1629176dcba03e7ec6
daddydirty420-app <newproject893420@gmail.com>
2026-05-18 12:04:22 +0900

    add: notificationList api fetch swr

--- client/src/app/notification/notificationList.tsx
+++ client/src/app/notification/notificationList.tsx
@@ -2,6 +2,9 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import useSWR from "swr";
+import { fetcher } from "../../lib/fetcher";
+import styles from "./styles.module.css";
 import { Notification } from "./type";
 
 type Response = {
@@ -15,8 +18,21 @@ export const NotificationList = () => {
     const router = useRouter();
 
     // APIフェッチ
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/notification`;
+
+    const { data, mutate } = useSWR<Response>(apiUrl, fetcher);
+
+    const notificationList = data?.notificationList;
+    const unreadCount = data?.unreadCount;
 
     return (
-        <></>
+        <>
+            <section className={styles.unreadCountSection}>
+                <small className={styles.unreadCountText}>
+                    未読
+                    <span className={styles.countSpan}>{unreadCount?.toLocaleString()}</span>件
+                </small>
+            </section>
+        </>
     );
 };
