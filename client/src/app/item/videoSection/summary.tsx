9d5b71b471809403776db14e19e0a456750a4e67
daddydirty420-app <newproject893420@gmail.com>
2026-05-21 10:57:45 +0900

    refactor: item fetchItemSortAdd api

--- client/src/app/item/videoSection/summary.tsx
+++ client/src/app/item/videoSection/summary.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { fetchItemSortAdd } from "../api/itemSection";
 import styles from "../itemCommon.module.css";
 import { Item } from "../itemPageTypes";
 
@@ -28,10 +29,7 @@ export const Summary = ({ id, item, sellerMe, page }: Props) => {
         setExpanded(!expanded);
         if (item.status === "soldout" || expanded || sellerMe || page !== "normal") return;
 
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/items/${id}/sort-number/add?number=5`, {
-            method: "PATCH",
-            headers: { "Content-type": "application/json" },
-        });
+        fetchItemSortAdd(id, 5);
     };
 
     return (
