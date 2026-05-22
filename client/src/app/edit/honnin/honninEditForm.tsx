4b3a79534d9fa32420482857a00001eef6fc7f95
daddydirty420-app <newproject893420@gmail.com>
2026-05-22 09:16:07 +0900

    hotfix: honnin fetchGetAddress インポート関数名修正

--- client/src/app/edit/honnin/honninEditForm.tsx
+++ client/src/app/edit/honnin/honninEditForm.tsx
@@ -11,7 +11,7 @@ import toast from "react-hot-toast";
 import { ApiError } from "../../../lib/api/apiError";
 import { sleep } from "../../../lib/sleep";
 import { showAddressErrorToast } from "../address/addressErrorMessage";
-import { getAddress } from "../api/address";
+import { fetchGetAddress } from "../api/address";
 import { fetchHonninSubmit } from "../api/honnin";
 import styles from "../edit.module.css";
 import EditUI from "../editUI";
@@ -67,7 +67,7 @@ export const HonninEditForm = ({ user, genderOptions, campaign }: Props) => {
         }
 
         try {
-            const address = await getAddress(postNumber);
+            const address = await fetchGetAddress(postNumber);
 
             setTodouhuken(address.todouhuken_name);
             setShikutyouson(address.shikutyouson);
