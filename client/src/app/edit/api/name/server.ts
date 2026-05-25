ccdd8a87e7c02906bee2270aaf64fb14ccf9ac14
daddydirty420-app <newproject893420@gmail.com>
2026-05-25 14:01:39 +0900

    add: edit name repName page api

--- client/src/app/edit/api/name/server.ts
+++ client/src/app/edit/api/name/server.ts
@@ -1,8 +1,13 @@
 import { apiFetchServer } from "../../../../lib/api/server";
-import { Name } from "../../type";
+import { Name, ShopInfo } from "../../type";
 
 type NamePageResponse = {
     name: Name;
+};
+
+type RepNameResponse = {
+    name: Name;
+    shop: ShopInfo;
 };
 
 export const fetchNamePage = async (): Promise<NamePageResponse> => {
@@ -25,6 +30,18 @@ export const fetchShopConNamePage = async (shopId: string): Promise<NamePageResp
 
 export const fetchShopEditConNamePage = async (shopEditId: string): Promise<NamePageResponse> => {
     return apiFetchServer(`/shop-info-edit/${shopEditId}/con-name`, {
+        cache: "no-store",
+    });
+};
+
+export const fetchShopRepNamePage = async (shopId: string): Promise<RepNameResponse> => {
+    return apiFetchServer(`/shop-info/${shopId}/rep-name`, {
+        cache: "no-store",
+    });
+};
+
+export const fetchShopEditRepNamePage = async (shopEditId: string): Promise<NamePageResponse> => {
+    return apiFetchServer(`/shop-info-edit/${shopEditId}/rep-name`, {
         cache: "no-store",
     });
 };
--- client/src/app/edit/name/shop/rep-name/signup/[id]/page.tsx
+++ client/src/app/edit/name/shop/rep-name/signup/[id]/page.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from "next";
-import { cookies } from "next/headers";
-import { notFound, redirect } from "next/navigation";
-import { Name } from "../../../../../type";
+import { fetchShopRepNamePage } from "../../../../../api/name/server";
 import { NameEditForm } from "../../../../nameEditForm";
 
 type Props = {
@@ -22,31 +20,11 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page({ params }: Props) {
     const { id } = await params;
 
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get("access-token")?.value;
-
-    if (!accessToken) redirect("/login");
-
-    const res = await fetch(`${process.env.API_URL}/shop-info/${id}/rep-name`, {
-        method: "GET",
-        cache: "no-store",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-        console.error(data.message);
-        notFound();
-    }
-
-    const name: Name = data.name;
+    const data = await fetchShopRepNamePage(id);
 
     return (
         <NameEditForm
-            name={name}
+            name={data.name}
             page="rep-shop-signup"
             shopId={id}
             idFrontUrl={data.shop.id_card_front}
