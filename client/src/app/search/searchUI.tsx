3a8c36116aa3d3e2307bb42e2205e3ffab636728
daddydirty420-app <newproject893420@gmail.com>
2026-06-02 09:00:03 +0900

    add: searchUI

---
+++ client/src/app/search/searchUI.tsx
@@ -0,0 +1,23 @@
+import { Back, Container } from "@/components";
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+};
+
+export default function SearchUI({ children }: Props) {
+    return (
+        <>
+            <Header />
+
+            <Container header>
+                <Back />
+                {children}
+            </Container>
+
+            <Footer />
+        </>
+    );
+}
