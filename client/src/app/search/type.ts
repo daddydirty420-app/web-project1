7f943835a98720587556b98c55d3e2dcc00a6fd0
daddydirty420-app <newproject893420@gmail.com>
2026-06-02 12:49:16 +0900

    add: search type.ts

---
+++ client/src/app/search/type.ts
@@ -0,0 +1,57 @@
+import { ItemAttributes } from "@/types/itemAttributes";
+
+export type Brand = {
+    id: string;
+    name: string;
+};
+
+export type Categories = {
+    id: string;
+    name: string;
+    level: number;
+    parent?: Categories | null;
+    allowed_gender: "men" | "women" | "both";
+    allowed_age: "adult" | "kids" | "both";
+};
+
+export type User = {
+    user_name: string;
+    profile_image: string;
+};
+
+export type Sale = {
+    id: string;
+    discount_rate: number;
+    discount_amount: number;
+    before_price: number;
+    sale_flag: boolean;
+};
+
+export type Video = {
+    id: string;
+    title: string;
+};
+
+export type Item = {
+    id: string;
+    name: string;
+    price: number;
+    status: "editing" | "draft" | "active" | "hidden" | "soldout" | "deleted";
+    gender_type: "men" | "women" | "unisex";
+    age_type: "adult" | "kids" | "both";
+    save_at: Date;
+    first_image_url: string;
+    attributes: ItemAttributes;
+    Video?: Video | null;
+    Sale?: Sale | null;
+    User?: User | null;
+    Categories?: Categories | null;
+    Brand?: Brand | null;
+};
+
+export type Search = {
+    id: string;
+    user_id: number;
+    search_text: string | null;
+    category_text: string | null;
+};
