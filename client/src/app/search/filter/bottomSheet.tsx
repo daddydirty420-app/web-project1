14e923923bcecee38f5452da30e39aa970c09f09
daddydirty420-app <newproject893420@gmail.com>
2026-06-09 13:56:05 +0900

    add: search menu icon bers

--- client/src/app/search/filter/bottomSheet.tsx
+++ client/src/app/search/filter/bottomSheet.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { ReactNode } from "react";
+import styles from "./styles.module.css";
 
 type Props = {
     children: ReactNode;
 };
 
 export const BottomSheet = ({ children }: Props) => {
-    return <>{children}</>;
+    return (
+        <>
+            <div className={styles.sheetOverlay} />
+            {children}
+        </>
+    );
 };
--- client/src/app/search/filter/filterMenu.tsx
+++ client/src/app/search/filter/filterMenu.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { BottomSheet } from "./bottomSheet";
 import { FilterContent } from "./filterContent";
 import { Popover } from "./popover";
+import styles from "./styles.module.css";
 
 type Props = {
     isDesktop: boolean;
 };
 
 export const FilterMenu = ({ isDesktop }: Props) => {
+    const [isDisplay, setIsDisplay] = useState(false);
+
     return (
         <>
+            <FontAwesomeIcon icon={faBars} className={styles.icon} onClick={() => setIsDisplay(!!isDisplay)} />
+
             {isDesktop && (
                 <Popover>
                     <FilterContent />
