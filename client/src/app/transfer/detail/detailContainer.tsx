5f320e8b9a5dae546337a9fb13f52465af9feb24
daddydirty420-app <newproject893420@gmail.com>
2026-06-16 19:33:04 +0900

    add: transfer detail detailContainer

---
+++ client/src/app/transfer/detail/detailContainer.tsx
@@ -0,0 +1,24 @@
+import { Container, TitleAndBack } from "@/components";
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import { ReactNode } from "react";
+
+type Props = {
+    title: string;
+    children: ReactNode;
+};
+
+export default function DetailContainer({ title, children }: Props) {
+    return (
+        <>
+            <Header />
+
+            <Container header>
+                <TitleAndBack title={title} url="/transfer/history" />
+                {children}
+            </Container>
+
+            <Footer />
+        </>
+    );
+}
