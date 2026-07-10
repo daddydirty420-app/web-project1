cec448521885f3a03d3f0dc25f048e8f036c02ca
daddydirty420-app <newproject893420@gmail.com>
2026-07-10 20:52:51 +0900

    clean: route uriagekin_lots /admin/180

--- server/src/routes/uriagekin_lots.ts
+++ server/src/routes/uriagekin_lots.ts
@@ -1,51 +1,6 @@
-import { subDays } from "date-fns";
 import { Router } from "express";
 import type { NextFunction, Request, Response } from "express-serve-static-core";
-import { Op } from "sequelize";
-import { authenticateToken, isAdmin } from "../middleware/index.js";
-import { BankAccount, UriagekinLots, User } from "../models/index.js";
 
 const router = Router();
-
-router.get(
-    "/admin/180",
-    authenticateToken,
-    isAdmin,
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const halfYearAgo = subDays(new Date(), 180);
-
-            const uriagekinList = await UriagekinLots.findAll({
-                where: {
-                    createdAt: { [Op.lt]: halfYearAgo },
-                },
-                order: [["createdAt", "ASC"]],
-                include: [
-                    {
-                        model: User,
-                        attributes: ["id", "user_name", "email"],
-                        include: [
-                            {
-                                model: BankAccount,
-                                attributes: ["id"],
-                            },
-                        ],
-                    },
-                ],
-            });
-
-            const totalUriagekin = uriagekinList.reduce((sum: number, data: any) => {
-                return sum + (data.uriagekin || 0);
-            }, 0);
-
-            res.json({
-                uriagekinList,
-                totalUriagekin,
-            });
-        } catch (err) {
-            next(err);
-        }
-    },
-);
 
 export default router;
