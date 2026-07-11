4057a65bd2b47e4e55ecdad23e4e5d5b3e0740d8
daddydirty420-app <newproject893420@gmail.com>
2026-07-11 16:35:56 +0900

    update: history/points type

--- client/src/app/history/points/type.ts
+++ client/src/app/history/points/type.ts
@@ -1,5 +1,11 @@
-export type PointHistory = {
+export type PointReasonOption = {
+    id: string;
+    name: string;
+};
+
+export type PointsHistory = {
     id: string;
     points: number;
-    
-}
+    createdAt: Date;
+    PointReasonOption: PointReasonOption;
+};
