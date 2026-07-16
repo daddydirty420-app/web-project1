0b65ae4290d504fcf278cf0d2c010a3bf7116dce
daddydirty420-app <newproject893420@gmail.com>
2026-07-16 18:25:08 +0900

    update: model uriagekin_history reason_id追加

--- server/src/models/uriagekin_history.ts
+++ server/src/models/uriagekin_history.ts
@@ -1,19 +1,23 @@
 import { Association, DataTypes, Model } from "sequelize";
 import sequelize from "../db.js";
 
+import UriagekinReasonOption from "./uriagekin_reason_option.js";
 import User from "./user.js";
 
 export class UriagekinHistory extends Model {
     declare id: number;
     declare uriagekin: number | null;
-    declare used_uriagekin: number | null;
     declare user_id: number;
+    declare reason_id: number;
     declare createdAt: Date;
     declare updatedAt: Date;
 
     static associate() {
         UriagekinHistory.belongsTo(User, {
             foreignKey: "user_id",
+        });
+        UriagekinHistory.belongsTo(UriagekinReasonOption, {
+            foreignKey: "reason_id",
         });
     }
 
@@ -31,11 +35,11 @@ UriagekinHistory.init(
             autoIncrement: true,
         },
         uriagekin: DataTypes.INTEGER,
-        used_uriagekin: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
         user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        reason_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
