5eb4d61f6c1679650e5edc16e079cd7e5fc37b62
daddydirty420-app <newproject893420@gmail.com>
2026-07-16 19:46:41 +0900

    add: rateLimit UraigekinHistory

--- server/src/routes/uriagekin_history.ts
+++ server/src/routes/uriagekin_history.ts
@@ -3,4 +3,9 @@ import type { NextFunction, Request, Response } from "express-serve-static-core"
 
 const router = Router();
 
+// GET /points-history?limit=number(&cursor="")
+// summary: ポイント履歴取得
+// page: /history/points
+router
+
 export default router;
