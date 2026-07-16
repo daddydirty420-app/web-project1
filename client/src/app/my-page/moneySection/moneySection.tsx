9fba46eff2fce6f55b71b027c0f2dec4d0e74d8e
daddydirty420-app <newproject893420@gmail.com>
2026-07-16 16:31:32 +0900

    clean: alert 日数計算不要箇所削除

--- client/src/app/my-page/moneySection/moneySection.tsx
+++ client/src/app/my-page/moneySection/moneySection.tsx
@@ -8,23 +8,6 @@ type Props = {
 };
 
 export const MoneySection = ({ user }: Props) => {
-    // alert残り日数
-    const setAfterDate = (date: Date | string): string => {
-        const now = new Date();
-        const d = typeof date === "string" ? new Date(date) : date;
-        const diffMs = d.getTime() - now.getTime();
-        const diffSec = Math.floor(diffMs / 1000);
-        const diffMin = Math.floor(diffSec / 60);
-        const diffHour = Math.floor(diffMin / 60);
-        const diffDay = Math.floor(diffHour / 24);
-
-        if (diffDay > 1) {
-            return `${diffDay}日`;
-        } else {
-            return "残りわずか";
-        }
-    };
-
     const pointLots = user.PointLots;
     const uriagekinLots = user.UriagekinLots;
 
