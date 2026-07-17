fc425d5d851744353dee789b787afb4ecae31515
daddydirty420-app <newproject893420@gmail.com>
2026-07-17 16:27:27 +0900

    update: company SITE使用

--- client/src/app/company/content.tsx
+++ client/src/app/company/content.tsx
@@ -1,39 +1,35 @@
 import { TokuteiSection } from "@/components/tokutei";
 import styles from "@/styles/tokutei.module.css";
 import Link from "next/link";
+import { SITE } from "../../config/site";
 
 export const Content = () => {
     return (
         <>
             <TokuteiSection header="事業者名">
-                <p>○○</p>
+                <p>{SITE.companyName}</p>
             </TokuteiSection>
 
             <TokuteiSection header="代表者">
-                <p>○○ ○○</p>
+                <p>{SITE.representative}</p>
             </TokuteiSection>
 
             <TokuteiSection header="創業">
-                <p>2026年○○月○○日</p>
+                <p>{SITE.createDate}</p>
             </TokuteiSection>
 
             <TokuteiSection header="所在地">
                 <div className="flex flex-start">
-                    <p className="break-all">〒210-0007</p>
-                    <p className="ml-[0.5rem] break-all">
-                        神奈川県川崎市川崎区駅前本町11-2
-                        <br />
-                        川崎フロンティアビル4階
-                    </p>
+                    <p className="break-all">{SITE.address}</p>
                 </div>
             </TokuteiSection>
 
             <TokuteiSection header="事業内容">
-                <p>WEBサービス・ECシステムの製作・運用</p>
+                <p>WEBサービス・ECシステムの開発・運用</p>
             </TokuteiSection>
 
             <TokuteiSection header="電話番号">
-                <p>請求があった場合、遅滞なく開示します。</p>
+                <p>{SITE.phone}</p>
                 <small className={styles.small}>
                     ※現在、お電話による対応は原則行っておりません。お問い合わせの際は、
                     <Link href="/inquiry" className="underline cursor-pointer">
@@ -45,9 +41,9 @@ export const Content = () => {
 
             <TokuteiSection header="メールアドレス">
                 <p>
-                    contact@○○.com
+                    {SITE.email}
                     <br />
-                    support@○○.com
+                    {SITE.supportEmail}
                 </p>
                 <small className={styles.small}>
                     ※現在、お電話による対応は原則行っておりません。お問い合わせの際は、
@@ -59,20 +55,20 @@ export const Content = () => {
             </TokuteiSection>
 
             <TokuteiSection header="営業時間">
-                <p>平日10～18時（お盆、年末年始期間を除く）</p>
+                <p>{SITE.contactTime}</p>
             </TokuteiSection>
 
             <TokuteiSection header="URL">
                 <p>
                     <Link href="/" className={styles.link}>
-                        https://○○.com
+                        {SITE.appLink}
                     </Link>
                     （仮）
                 </p>
             </TokuteiSection>
 
             <TokuteiSection header="取引銀行">
-                <p>○○銀行</p>
+                <p>{SITE.bank}</p>
             </TokuteiSection>
         </>
     );
--- client/src/config/site.ts
+++ client/src/config/site.ts
@@ -2,11 +2,13 @@ export const SITE = {
     companyName: "マジックミラースタジアム株式会社",
     representative: "ラミン・アナル",
     email: "anal@nullnull.peropero",
+    supportEmail: "nullnull@nullnull.sitainoyo~n",
     phone: "420-893-666",
     address: "〒893-1919 東京都川崎市横浜6-666 null404階",
 
-    siteLink: "https://app.fuckintesting.com",
-    siteName: "Coming Soon",
+    appLink: "https://app.fuckintesting.com",
+    appName: "Coming Soon",
+    createDate: "2024年9月2日",
     startDate: "2026年6月66日",
 
     bank: "FUCK銀行",
