84191aad0199d1d1c8c60bbffdcab38e3737670d
daddydirty420-app <newproject893420@gmail.com>
2026-07-19 18:43:45 +0900

    add: personal-information page

---
+++ client/src/app/personal-information/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import { SITE } from "../../config/site";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { redirect } from "next/navigation";
+import PersonalInformationUI from "./personalInfomationUI";
+import { LinkSection } from "./linkSection";
+
+export const metadata: Metadata = {
+    title: "個人情報設定",
+    description: `${SITE.appName}をご利用の際の各種個人情報・ログイン情報はこちらから編集ページへアクセスできます。`,
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
+
+export default async function Page() {
+    const session = await getServerSession(authOptions);
+
+    if (!session) redirect("/login");
+
+    return (
+        <PersonalInformationUI title="個人情報設定">
+            <LinkSection />
+        </PersonalInformationUI>
+    )
+}
