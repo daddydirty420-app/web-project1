446becea49bd287991cc73901b014b59f0cf044d
daddydirty420-app <newproject893420@gmail.com>
2026-07-19 18:07:49 +0900

    add: PersonalInfmationUI

---
+++ client/src/app/personal-information/personalInfomationUI.tsx
@@ -0,0 +1,24 @@
+import { Container, TitleAndBack } from "@/components";
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import { ReactNode } from "react";
+
+type Props = {
+    title: string;
+    children: ReactNode;
+};
+
+export default function PersonalInformationUI({ title, children }: Props) {
+    return (
+        <>
+            <Header />
+
+            <Container header>
+                <TitleAndBack title={title} />
+                {children}
+            </Container>
+
+            <Footer />
+        </>
+    );
+}
