33b9febaef55f495301b12ef7e72f3ba11dfc677
daddydirty420-app <newproject893420@gmail.com>
2026-07-22 13:25:38 +0900

    update: signup resend loading sleep運用変更

--- client/src/app/signup/verify/resend.tsx
+++ client/src/app/signup/verify/resend.tsx
@@ -2,19 +2,26 @@
 
 import styles from "@/styles/login.module.css";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 import { ApiError } from "../../../lib/api/apiError";
+import { sleep } from "../../../lib/sleep";
 import { fetchResend } from "../api/auth";
 
 export const Resend = () => {
+    const [loading, setLoading] = useState(false);
+
     const router = useRouter();
     const searchParams = useSearchParams();
 
     const handleResend = async () => {
+        setLoading(true);
+
         const token = searchParams.get("token");
 
         if (!token) {
             toast.error("再発行用トークンが見つかりません");
+            setLoading(false);
             return;
         }
 
@@ -22,9 +29,12 @@ export const Resend = () => {
             const data = await fetchResend(token);
 
             toast.success("認証コードを再発行しました");
+            setLoading(false);
+            await sleep(1500);
 
             router.push(data.reissueUrl);
         } catch (err) {
+            setLoading(false);
             if (err instanceof ApiError) {
                 toast.error("認証コードの再発行に失敗しました");
                 return;
@@ -36,7 +46,7 @@ export const Resend = () => {
 
     return (
         <p className={styles.reference} onClick={handleResend}>
-            認証コードを再発行する
+            {loading ? "認証コード発行中..." : "認証コードを再発行する"}
         </p>
     );
 };
