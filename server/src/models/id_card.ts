79ece721d632960f2c5e6371a86085510c5a19a9
daddydirty420-app <newproject893420@gmail.com>
2026-07-24 16:37:34 +0900

    refactor: model idcard リレーションカラム削除

--- server/src/models/id_card.ts
+++ server/src/models/id_card.ts
@@ -1,36 +1,23 @@
 import { Association, DataTypes, Model } from "sequelize";
 import sequelize from "../db.js";
 
-import ShopInfo from "./shop_info.js";
-import ShopInfoEdit from "./shop_info_edit.js";
 import User from "./user.js";
 
 export class IdCard extends Model {
     declare id: number;
     declare id_card_front: string | null;
     declare id_card_rear: string | null;
-    declare user_id: number | null;
-    declare shop_info_id: number | null;
-    declare shop_info_edit_id: number | null;
     declare createdAt: Date;
     declare updatedAt: Date;
 
     static associate() {
-        IdCard.belongsTo(User, {
+        IdCard.hasOne(User, {
             foreignKey: "user_id",
-        });
-        IdCard.belongsTo(ShopInfo, {
-            foreignKey: "shop_info_id",
-        });
-        IdCard.belongsTo(ShopInfoEdit, {
-            foreignKey: "shop_info_edit_id",
         });
     }
 
     static associations: {
         User: Association<IdCard, User>;
-        ShopInfo: Association<ShopInfo, User>;
-        ShopInfoEdit: Association<ShopInfoEdit, User>;
     };
 }
 
@@ -44,18 +31,6 @@ IdCard.init(
         },
         id_card_front: DataTypes.TEXT,
         id_card_rear: DataTypes.TEXT,
-        user_id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-        },
-        shop_info_id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-        },
-        shop_info_edit_id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-        },
     },
     {
         sequelize,
