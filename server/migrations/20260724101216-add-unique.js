9bac07d5cf5a5a917e233276a7450adb952a9385
daddydirty420-app <newproject893420@gmail.com>
2026-07-24 20:52:19 +0900

    release: 7/24 作業完了
    リレーション関係変更データ　データ入力
    
    transfer account_id削除マイグレーション
    
    IdCard リレーション変更
    
    旧リレーションカラム削除
    
    unique制約追加
    
    README更新

--- server/migrations/20260724101216-add-unique.js
+++ server/migrations/20260724101216-add-unique.js
@@ -79,63 +79,24 @@ export default {
 
     async down(queryInterface, Sequelize) {
         // user
-        await queryInterface.removeConstraint(
-            "user",
-            "user_address_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "user",
-            "user_name_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "user",
-            "user_account_id_unique"
-        );
+        await queryInterface.removeConstraint("user", "user_address_id_unique");
+        await queryInterface.removeConstraint("user", "user_name_id_unique");
+        await queryInterface.removeConstraint("user", "user_account_id_unique");
 
         // delivery
-        await queryInterface.removeConstraint(
-            "delivery",
-            "delivery_address_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "delivery",
-            "delivery_account_id_unique"
-        );
+        await queryInterface.removeConstraint("delivery", "delivery_address_id_unique");
+        await queryInterface.removeConstraint("delivery", "delivery_account_id_unique");
 
         // shop_info
-        await queryInterface.removeConstraint(
-            "shop_info",
-            "shop_info_name_representative_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info",
-            "shop_info_name_contact_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info",
-            "shop_info_address_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info",
-            "shop_info_account_id_unique"
-        );
+        await queryInterface.removeConstraint("shop_info", "shop_info_name_representative_id_unique");
+        await queryInterface.removeConstraint("shop_info", "shop_info_name_contact_id_unique");
+        await queryInterface.removeConstraint("shop_info", "shop_info_address_id_unique");
+        await queryInterface.removeConstraint("shop_info", "shop_info_account_id_unique");
 
         // shop_info_edit
-        await queryInterface.removeConstraint(
-            "shop_info_edit",
-            "shop_info_edit_name_representative_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info_edit",
-            "shop_info_edit_name_contact_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info_edit",
-            "shop_info_edit_address_id_unique"
-        );
-        await queryInterface.removeConstraint(
-            "shop_info_edit",
-            "shop_info_edit_account_id_unique"
-        );
+        await queryInterface.removeConstraint("shop_info_edit", "shop_info_edit_name_representative_id_unique");
+        await queryInterface.removeConstraint("shop_info_edit", "shop_info_edit_name_contact_id_unique");
+        await queryInterface.removeConstraint("shop_info_edit", "shop_info_edit_address_id_unique");
+        await queryInterface.removeConstraint("shop_info_edit", "shop_info_edit_account_id_unique");
     },
 };
