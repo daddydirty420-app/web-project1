3536b637e2a8404637f94f67e19c95299637abdb
daddydirty420-app <newproject893420@gmail.com>
2026-07-30 20:42:54 +0900

    add: model couponCategory column

--- server/src/models/coupon_category.ts
+++ server/src/models/coupon_category.ts
@@ -1,4 +1,5 @@
-import { Association, Model } from "sequelize";
+import { Association, DataTypes, Model } from "sequelize";
+import sequelize from "../db.js";
 
 import Categories from "./categories.js";
 import Coupon from "./coupon.js";
@@ -24,5 +25,31 @@ export class CouponCategory extends Model {
         ShopInfo: Association<CouponCategory, Categories>;
     };
 }
+
+CouponCategory.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        coupon_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: "CouponCategory",
+        tableName: "coupon_category",
+        freezeTableName: true,
+        timestamps: true,
+    },
+);
 
 export default CouponCategory;
