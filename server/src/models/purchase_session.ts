2e9af9cece3ea9b9203d7521d68e13b3401bf3ce
daddydirty420-app <newproject893420@gmail.com>
2026-07-30 20:58:00 +0900

    add: model purchaseSession column

--- server/src/models/purchase_session.ts
+++ server/src/models/purchase_session.ts
@@ -1,4 +1,6 @@
-import { Association, Model } from "sequelize";
+import { Association, DataTypes, Model } from "sequelize";
+import sequelize from "../db.js";
+
 import Address from "./address.js";
 import CouponUser from "./coupon_user.js";
 import Item from "./item.js";
@@ -21,7 +23,6 @@ export class PurchaseSession extends Model {
     declare coupon_user_id: number | null;
     declare buyer_phone_number: string | null;
     declare item_count: number | null; // 購入個数
-    declare discount_amount: number | null; // 割引（クーポン等）
     declare points_used: number | null; // ポイント払い額
     declare payment_method_id: number | null;
     declare selected_variant: SelectedVariant;
@@ -60,5 +61,54 @@ export class PurchaseSession extends Model {
         PaymentMethodOption: Association<PurchaseSession, PaymentMethodOption>;
     };
 }
+
+PurchaseSession.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        buyer_user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        item_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        address_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        name_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        coupon_user_id: DataTypes.INTEGER,
+        buyer_phone_number: DataTypes.STRING,
+        item_count: DataTypes.INTEGER,
+        points_used: DataTypes.INTEGER,
+        payment_method_id: DataTypes.INTEGER,
+        selected_variant: {
+            type: DataTypes.JSONB,
+            allowNull: false,
+            defaultValue: {},
+        },
+        arrive_specified_date: DataTypes.DATE,
+        expires_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: "PurchaseSession",
+        tableName: "purchase_session",
+        freezeTableName: true,
+        timestamps: true,
+    },
+);
 
 export default PurchaseSession;
