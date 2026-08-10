ecae3c85bcd413e504db2d9a147c6397a797df95
daddydirty420-app <newproject893420@gmail.com>
2026-08-10 14:13:37 +0900

    fix: deleteUserAdmin 口座間違い修正

--- server/src/types/bankSnapshot.ts
+++ server/src/types/bankSnapshot.ts
@@ -5,7 +5,7 @@ export type AccountType = (typeof ACCOUNT_TYPES)[number];
 export type BankSnapshot = {
     bank_name: string;
     branch_name: string;
-    account_type: AccountType;
+    account_type: AccountType | "";
     account_number: string;
     meigi: string;
 };
