7c7d77b8f161e4dae402f60345f1f5ab43073075
daddydirty420-app <newproject893420@gmail.com>
2026-08-28 14:01:08 +0900

    add: migration create-permit-file

--- server/migrations/20260828045642-create-permit-file.js
+++ server/migrations/20260828045642-create-permit-file.js
@@ -1,22 +1,62 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+export default {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("permit_file", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            permit_id: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                references: {
+                    model: "permit",
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            s3_metadata_id: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                unique: true,
+                references: {
+                    model: "s3_metadata",
+                    key: "id",
+                },
+                onUpdate: "NO ACTION",
+                onDelete: "NO ACTION",
+            },
+            sort_order: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            document_name: {
+                type: Sequelize.STRING(255),
+                allowNull: true,
+            },
+            memo: {
+                type: Sequelize.STRING(255),
+                allowNull: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+            },
+        });
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("permit_file");
+    },
 };
