3dc23f59f791723ab32528c66b928760b15d28fa
Rednull Racing <newproject893420@gmail.com>
2026-09-02 18:17:13 +0900

    update: server honnin id_cardカラム変更に伴う設計変更

--- server/src/usecases/users/edit/honnin.ts
+++ server/src/usecases/users/edit/honnin.ts
@@ -1,15 +1,16 @@
 import sequelize from "../../../db.js";
 import { AppError } from "../../../errors.js";
-import { publicS3Domain } from "../../../infra/aws/s3.js";
+import { deleteS3Object } from "../../../infra/aws/deleteS3Object.js";
+import { buckets } from "../../../infra/aws/s3.js";
+import { uploadS3Object } from "../../../infra/aws/uploadS3Object.js";
 import { updateAddress } from "../../../services/address.js";
-import { createIdCard, updateIdCard } from "../../../services/idCard.js";
+import { createIdCard, deleteIdCard } from "../../../services/idCard.js";
 import { updateName } from "../../../services/name.js";
 import { createNotification } from "../../../services/notification.js";
+import { createS3Metadata, deleteS3Metadata, getS3Metadata } from "../../../services/s3Metadata.js";
 import { getTodouhukenOne } from "../../../services/todouhuken.js";
 import { updateHonninUser, updateIdCardIdUser } from "../../../services/users/command.js";
 import { getUserWithAddressNameId } from "../../../services/users/query.js";
-import { deleteCmdS3 } from "../../../utils/s3/deleteCmd.js";
-import { generateSignedUrl } from "../../../utils/s3/signedUrl.js";
 import { HonninBody } from "../../../validators/body/users.js";
 
 type Params = {
@@ -36,12 +37,10 @@ export const editHonninUserUseCase = async ({ userId, body }: Params) => {
         building,
         phoneNumber,
         selectedGender,
-        frontFileName,
-        frontFileType,
-        rearFileName,
-        rearFileType,
         idFrontUpload,
         idRearUpload,
+        frontIdCard,
+        rearIdCard,
     } = body;
 
     // 空チェック
@@ -82,115 +81,164 @@ export const editHonninUserUseCase = async ({ userId, body }: Params) => {
         throw new AppError("INVALID_TODOUHUKEN", 400);
     }
 
-    // 身分証url発行
-    let frontSignedUrl: string | null = null;
-    let rearSignedUrl: string | null = null;
-    let frontUrl: string | null = null;
-    let rearUrl: string | null = null;
-    const oldFrontUrl = user.IdCard?.id_card_front || null;
-    const oldRearUrl = user.IdCard?.id_card_rear || null;
+    if (!idFrontUpload || !frontIdCard) throw new AppError("INVALID_BODY", 400);
+    if (!idRearUpload || !rearIdCard) throw new AppError("INVALID_BODY", 400);
 
-    if (frontFileName && frontFileType && idFrontUpload) {
-        const key = `idcard/front/${userId}/${now}_${frontFileName}`;
+    const oldIdCard = user.IdCard ?? null;
+    const oldFrontS3Metadata = oldIdCard?.front_s3_metadata_id
+        ? await getS3Metadata({ s3MetadataId: oldIdCard.front_s3_metadata_id })
+        : null;
+    const oldRearS3Metadata = oldIdCard?.rear_s3_metadata_id
+        ? await getS3Metadata({ s3MetadataId: oldIdCard.rear_s3_metadata_id })
+        : null;
 
-        frontSignedUrl = await generateSignedUrl({ key, contentType: frontFileType });
+    const uploadedObjects: Array<{
+        bucketName: string;
+        objectKey: string;
+        versionId: string | null;
+        etag: string | null;
+        fileName: string;
+        contentType: string;
+        size: number;
+        type: "front" | "rear";
+    }> = [];
+    let committed = false;
 
-        frontUrl = `${publicS3Domain}/${key}`;
-    }
-
-    if (rearFileName && rearFileType && idRearUpload) {
-        const key = `idcard/rear/${userId}/${now}_${rearFileName}`;
-
-        rearSignedUrl = await generateSignedUrl({ key, contentType: rearFileType });
-
-        rearUrl = `${publicS3Domain}/${key}`;
-    }
-
-    // 旧身分証URL削除
-    if (idFrontUpload && oldFrontUrl && frontUrl && frontUrl !== oldFrontUrl) {
-        const oldFrontKey = oldFrontUrl.split(".com/")[1];
-
-        deleteCmdS3({ key: oldFrontKey }).catch((err) => {
-            console.error("s3 deleteCmdS3 error:", err);
+    try {
+        const uploadedFront = await uploadS3Object({
+            bucketName: buckets.verificationDocuments,
+            objectKey: `idcard/front/${userId}/${now}_${frontIdCard.fileName}`,
+            body: frontIdCard.buffer,
+            contentType: frontIdCard.contentType,
         });
-    }
+        uploadedObjects.push({ ...uploadedFront, ...frontIdCard, type: "front" });
 
-    if (idRearUpload && oldRearUrl && rearUrl && rearUrl !== oldRearUrl) {
-        const oldRearKey = oldRearUrl.split(".com/")[1];
-
-        deleteCmdS3({ key: oldRearKey }).catch((err) => {
-            console.error("s3 deleteCmdS3 error:", err);
+        const uploadedRear = await uploadS3Object({
+            bucketName: buckets.verificationDocuments,
+            objectKey: `idcard/rear/${userId}/${now}_${rearIdCard.fileName}`,
+            body: rearIdCard.buffer,
+            contentType: rearIdCard.contentType,
         });
-    }
+        uploadedObjects.push({ ...uploadedRear, ...rearIdCard, type: "rear" });
 
-    // DB更新
-    await sequelize.transaction(async (t) => {
-        if (frontUrl && rearUrl) {
-            if (user.IdCard) {
-                await updateIdCard({
-                    idCard: user.IdCard,
+        await sequelize.transaction(async (t) => {
+            let frontS3MetadataId: number | null = null;
+            let rearS3MetadataId: number | null = null;
+
+            for (const uploadedObject of uploadedObjects) {
+                const s3Metadata = await createS3Metadata({
                     data: {
-                        id_card_front: frontUrl,
-                        id_card_rear: rearUrl,
-                    },
-                    transaction: t,
-                });
-            } else {
-                const newIdCard = await createIdCard({
-                    data: {
-                        id_card_front: frontUrl,
-                        id_card_rear: rearUrl,
+                        bucket_name: uploadedObject.bucketName,
+                        object_key: uploadedObject.objectKey,
+                        version_id: uploadedObject.versionId,
+                        original_file_name: uploadedObject.fileName,
+                        content_type: uploadedObject.contentType,
+                        file_size: uploadedObject.size,
+                        etag: uploadedObject.etag,
                     },
                     transaction: t,
                 });
 
-                await updateIdCardIdUser({
-                    user,
-                    data: { idcard_id: newIdCard.id },
+                if (uploadedObject.type === "front") frontS3MetadataId = s3Metadata.id;
+                if (uploadedObject.type === "rear") rearS3MetadataId = s3Metadata.id;
+            }
+
+            if (!frontS3MetadataId || !rearS3MetadataId) throw new AppError("INVALID_BODY", 400);
+
+            const newIdCard = await createIdCard({
+                data: {
+                    front_s3_metadata_id: frontS3MetadataId,
+                    rear_s3_metadata_id: rearS3MetadataId,
+                },
+                transaction: t,
+            });
+
+            await updateIdCardIdUser({
+                user,
+                data: { idcard_id: newIdCard.id },
+                transaction: t,
+            });
+
+            if (oldIdCard) await deleteIdCard({ idCard: oldIdCard, transaction: t });
+
+            if (user.Address) {
+                await updateAddress({
+                    address: user.Address,
+                    data: {
+                        post_number: postNumber,
+                        todouhuken_id: todouhukenId,
+                        shikutyouson: shikutyouson,
+                        banchi: banchi,
+                        building: building,
+                    },
                     transaction: t,
                 });
             }
-        }
 
-        if (user.Address) {
-            await updateAddress({
-                address: user.Address,
+            if (user.Name) {
+                await updateName({
+                    name: user.Name,
+                    data: {
+                        sei: sei,
+                        mei: mei,
+                        sei_kana: seiKana,
+                        mei_kana: meiKana,
+                    },
+                    transaction: t,
+                });
+            }
+
+            await updateHonninUser({
+                user,
                 data: {
-                    post_number: postNumber,
-                    todouhuken_id: todouhukenId,
-                    shikutyouson: shikutyouson,
-                    banchi: banchi,
-                    building: building,
+                    honnin_verify_request: true,
+                    honnin_verified: false,
+                    birthday: formattedBirthday,
+                    phone_number: phoneNumber,
+                    gender_id: Number(selectedGender),
                 },
                 transaction: t,
             });
-        }
-
-        if (user.Name) {
-            await updateName({
-                name: user.Name,
-                data: {
-                    sei: sei,
-                    mei: mei,
-                    sei_kana: seiKana,
-                    mei_kana: meiKana,
-                },
-                transaction: t,
-            });
-        }
-
-        await updateHonninUser({
-            user,
-            data: {
-                honnin_verify_request: true,
-                honnin_verified: false,
-                birthday: formattedBirthday,
-                phone_number: phoneNumber,
-                gender_id: Number(selectedGender),
-            },
-            transaction: t,
         });
-    });
+
+        committed = true;
+    } catch (err) {
+        if (!committed) {
+            await Promise.allSettled(
+                uploadedObjects.map((object) =>
+                    deleteS3Object({
+                        bucketName: object.bucketName,
+                        objectKey: object.objectKey,
+                        versionId: object.versionId,
+                    }),
+                ),
+            );
+        }
+
+        throw err;
+    }
+
+    const replacedS3Metadata = [oldFrontS3Metadata, oldRearS3Metadata].filter(
+        (s3Metadata) => s3Metadata !== null,
+    );
+
+    if (replacedS3Metadata.length > 0) {
+        await sequelize.transaction(async (t) => {
+            for (const s3Metadata of replacedS3Metadata) {
+                await deleteS3Metadata({ s3Metadata, transaction: t });
+            }
+        });
+
+        await Promise.allSettled(
+            replacedS3Metadata.map((s3Metadata) =>
+                deleteS3Object({
+                    bucketName: s3Metadata.bucket_name,
+                    objectKey: s3Metadata.object_key,
+                    versionId: s3Metadata.version_id,
+                }),
+            ),
+        );
+    }
 
     // お知らせ作成
     createNotification({
@@ -203,6 +251,4 @@ export const editHonninUserUseCase = async ({ userId, body }: Params) => {
     }).catch((err) => {
         console.error("service createNotification error:", err);
     });
-
-    return { frontSignedUrl, rearSignedUrl };
 };
